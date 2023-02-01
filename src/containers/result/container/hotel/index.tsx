42d1625b7602434bd58aa2d2a407def7c18b23a0
Bibin Antony <bibin@simplogics.com>
2023-02-01 17:04:27 +0530

    places

---
+++ src/containers/result/container/hotel/index.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Locations } from "../../../../locations";
+
+export const HotelList: FunctionComponent = () => {
+	const navigate = useNavigate();
+    
+	return (
+		<div className="placeHolder">
+			This feature is Under development
+			<button className="primary_border_btn destination_btn mt-2" onClick={() => navigate(Locations.RESULT)}>Go back</button>
+		</div>
+	);
+};
+export default HotelList;
